f83ee753850b9c80ea85df60993712fae179a2b5
Arnav Gupta <championswimmer@gmail.com>
2022-01-22 22:15:14 +0530

    handle wordsize and attempts
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- lib/widgets/wordle_key.dart
+++ lib/widgets/wordle_key.dart
@@ -8,31 +8,31 @@ class WordleKey extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Widget keyCap;
-    double width = 60;
+    double width = 50;
     if (letter == "_") {
       keyCap = Icon(
         Icons.keyboard_return,
-        size: 20,
+        size: 18,
       );
     } else if (letter == "<") {
       keyCap = Icon(
         Icons.backspace_outlined,
-        size: 20,
+        size: 18,
       );
     } else {
-      width = 40;
+      width = 35;
       keyCap = Text(
         letter,
-        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
       );
     }
 
     return Container(
       width: width,
-      height: 60,
+      height: 55,
       alignment: Alignment.center,
       // padding: EdgeInsets.all(10),
-      margin: EdgeInsets.all(4),
+      margin: EdgeInsets.all(3),
       decoration: BoxDecoration(
           borderRadius: BorderRadius.all(Radius.circular(4)),
           color: Color.fromARGB(44, 44, 44, 44)),
