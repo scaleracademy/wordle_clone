457838a0c0402d00fa98956be44cbf8989fb061c
Arnav Gupta <championswimmer@gmail.com>
2022-02-02 03:52:10 +0530

    use the HTML renderer
    Signed-off-by: Arnav Gupta <championswimmer@gmail.com>

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,11 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordle_clone/widgets/wordle_page.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  const providerScopedApp =  ProviderScope(child: MyApp());
+  FlutterError.onError = (FlutterErrorDetails details) {
+
+  };
+  runApp(providerScopedApp);
 }
 
 class MyApp extends StatelessWidget {
@@ -15,15 +19,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const WordlePage(),
